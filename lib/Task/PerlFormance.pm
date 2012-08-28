902acbc2d18cc266cd2addc44b53bcd184e282f5
Steffen Schwigon <ss5@renormalist.net>
2012-08-28 16:39:47 +0200

    max deps for Benchmark::Perl::Formance

---
+++ lib/Task/PerlFormance.pm
@@ -0,0 +1,123 @@
+use strict;
+use warnings;
+package Task::PerlFormance;
+# ABSTRACT: Max dependencies for Benchmark::Perl::Formance
+
+
+=pkgroup perlformance
+
+
+=pkg forks
+
+=pkg Benchmark::Perl::Formance
+
+=pkg Tapper::TestSuite::Benchmark::Perl::Formance
+
+
+
+=pkgroup SpamAssassin
+
+
+=pkg DB_File
+
+=pkg Net::IP
+
+=pkg Net::DNS
+
+=pkg NetAddr::IP
+
+=pkg HTML::Parser
+
+=pkg Mail::SpamAssassin
+
+
+
+=pkgroup OO
+
+
+=pkg MooseX::Declare
+
+=pkg Moose
+
+=pkg Mouse
+
+
+
+=pkgroup RxCmp
+
+
+=pkg POSIX::Regex
+
+=pkg ExtUtils::CppGuess
+
+=pkg re::engine::Lua
+
+=pkg re::engine::LPEG
+
+=pkg re::engine::Plan9
+
+=pkg re::engine::Oniguruma
+
+=pkg re::engine::RE2
+
+
+
+=pkgroup Regex
+
+
+=pkg Regexp::Common
+
+=pkg DateTime::Calendar::Mayan
+
+=pkg Locale::US
+
+=pkg HTTP::Headers
+
+=pkg URI
+
+
+
+=pkgroup Shootout
+
+
+=pkg Math::GMP
+
+
+=pkgroup Primes
+
+=pkg Crypt::Primes
+
+=pkg Math::Primality
+
+
+
+=pkgroup DPath
+
+
+=pkg Clone
+
+=pkg Devel::Size
+
+=pkg Data::DPath
+
+
+
+=pkgroup P6STD
+
+
+=pkg Text::Balanced
+
+=pkg YAML::XS
+
+=pkg Encode
+
+
+
+=pkgroup PerlCritic
+
+
+=pkg Perl::Critic
+
+
+
+1;
